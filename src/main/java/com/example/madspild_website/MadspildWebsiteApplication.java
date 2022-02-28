ce190c57d786fd960ef33a7fdd9cf617fb671a30
gulernurdonmez <gule0153@stud.kea.dk>
2022-02-28 10:40:19 +0100

    first commit

---
+++ src/main/java/com/example/madspild_website/MadspildWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.madspild_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadspildWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MadspildWebsiteApplication.class, args);
+    }
+
+}
