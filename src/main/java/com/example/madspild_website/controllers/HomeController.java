1bf36cfbd6e0248eb9102502a1d6aca8270ca755
gulernurdonmez <gule0153@stud.kea.dk>
2022-03-02 12:50:06 +0100

    changed getmapping link

--- src/main/java/com/example/madspild_website/controllers/HomeController.java
+++ src/main/java/com/example/madspild_website/controllers/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/frontPage")
+    @GetMapping()
     public String frontPage(){
         return "frontPage";
     }
