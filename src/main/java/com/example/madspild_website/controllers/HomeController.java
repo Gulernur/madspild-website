118e4e8139db1e50cfdee042483c9dc8e6f370c4
gulernurdonmez <gule0153@stud.kea.dk>
2022-03-03 11:10:05 +0100

    added link to front page in about page

--- src/main/java/com/example/madspild_website/controllers/HomeController.java
+++ src/main/java/com/example/madspild_website/controllers/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping()
+    @GetMapping("/frontPage")
     public String frontPage(){
         return "frontPage";
     }
